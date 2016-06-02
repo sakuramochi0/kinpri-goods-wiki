f89f34012473c235769036550cb65cdc918de581
sakuramochi <sakuramochi.mochi.0@gmail.com>
2016-06-02 21:17:29 +0900

    Add print program for Wiki format.

---
+++ print_release_date_list.py
@@ -0,0 +1,16 @@
+import datetime
+from dateutil.parser import parse
+from urllib.parse import unquote
+from pymongo.mongo_client import MongoClient
+
+cli = MongoClient()
+c = cli.kinpri_goods_wiki.items
+
+for i in c.find({'date': {'$gte': parse('2015-1-1')}}).sort('date'):
+    if i['date_extra']:
+        date = i['date'].strftime('%m月{}旬'.format(i['date_extra']))
+    else:
+        date = i['date'].strftime('%m月%d日')
+    name = i['name'].replace('KING OF PRISM by PrettyRhythm', '').replace('KING OF PRISM', '').strip()
+    page = unquote(i['url'].split('/')[-1], encoding='euc-jp')
+    print('| {} | [[{}>{}]] |  |'.format(date, name, page))
