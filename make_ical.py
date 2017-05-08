deb650031e87bbba62b6365dd2d64d0b046a3426
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-05-08 22:52:52 +0900

    add category label to summary and refactoring

--- make_ical.py
+++ make_ical.py
@@ -9,43 +9,64 @@ c = cli.kinpri_goods_wiki.items
 cal = Calendar()
 for i in c.find():
     e = Event()
-    if i['date']:
-        date = i['date'].date()
+    if not i['date']:
+        continue
+    date = i['date'].date()
 
-        # date_extra
-        if i['date_extra']:
-            if i['date_extra'] == '月':
-                day = 1
-                date_extra_description = '※この商品の発売日は{month}月となっています。\n'.format(
-                    month=date.month,
-                    extra=i['date_extra'],
-                )
-            else:
-                if i['date_extra'] == '上':
-                    day = 1
-                elif i['date_extra'] == '中':
-                    day = 10
-                elif i['date_extra'] == '下':
-                    day = 20
-                    date_extra_description = '※この商品の発売日は{month}月{extra}旬となっています。\n'.format(
-                        month=date.month,
-                        extra=i['date_extra'],
-                    )
+    # date_extra
+    if i['date_extra']:
+        if i['date_extra'] == '月':
+            day = 1
+            date_extra_description = '※この商品の発売日は、この日ではなく、{month}月中となっています。\n'.format(
+                month=date.month,
+                extra=i['date_extra'],
+            )
         else:
-            day = date.day
-            date_extra_description = ''
+            if i['date_extra'] == '上':
+                day = 1
+            elif i['date_extra'] == '中':
+                day = 10
+            elif i['date_extra'] == '下':
+                day = 20
+            date_extra_description = '※この商品の発売日は、この日ではなく、{month}月{extra}旬となっています。\n'.format(
+                month=date.month,
+                extra=i['date_extra'],
+            )
+    else:
+        day = date.day
+        date_extra_description = ''
 
-        date = datetime.date(date.year, date.month, day)
+    date = datetime.date(date.year, date.month, day)
+    
+    tags = ' '.join(i['tags'])
+    summary = '[発売日]'
+    if '雑誌' in tags:
+        summary += '[雑誌]'
+    elif 'コミックス' in tags or 'アンソロジー' in tags:
+        summary += '[コミックス]'
+    elif '映像' in tags:
+        summary += '[DVD]'
+    elif '円盤' in tags:
+        summary += '[CD]'
+    elif 'プリティーリズム' in tags:
+        summary += '[プリティーリズム]'
+    else:
+        summary = ''
+        summary += ' ' + i['name']
         
-        e.add('dtstart', date)
-        e.add('summary', '[発売日] ' + i['name'])
-        e.add('description', '{date_extra_description}発売元: {maker}\nWiki: {url}'.format(
-            maker=i['maker'],
-            url=i['url'],
-            date_extra_description=date_extra_description,
-        ))
-        cal.add_component(e)
+    description = '''{date_extra_description}発売元: {maker}
+タグ: {tags}
+Wiki: {url}'''.format(
+    maker=i['maker'],
+    tags=tags,
+    url=i['url'],
+    date_extra_description=date_extra_description,
+)
+    e.add('dtstart', date)
+    e.add('summary', summary)
+    e.add('description', description)
+    cal.add_component(e)
 
-with open('calendar.ics', 'wb') as f:
+with open('kinpri-goods-wiki.ics', 'wb') as f:
     f.write(cal.to_ical())
 
