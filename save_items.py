db1b934a33801a8d3c8515463c57ef9b3739fa16
sakuramochi <sakuramochi.mochi.0@gmail.com>
2018-09-21 21:32:39 +0900

    リファクタリング

--- save_items.py
+++ save_items.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import re
-import time
-import datetime
-from dateutil.parser import parse
 import argparse
-from urllib.parse import unquote_plus
-from bs4 import BeautifulSoup
+import datetime
+import re
+
 import requests
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
 from get_mongo_client import get_mongo_client
 
 BASE_URL = 'http://kinprigoods.memo.wiki/'
@@ -25,12 +24,15 @@ IGNORE_URLS = [
     # 発売日リスト
     'd/%c8%af%c7%e4%c6%fc%a5%ea%a5%b9%a5%c8',
     # キャンペーン・期間限定ショップ・イベント販売情報
-    'd/%a5%ad%a5%e3%a5%f3%a5%da%a1%bc%a5%f3%a1%a6%b4%fc%b4%d6%b8%c2%c4%ea%a5%b7%a5%e7%a5%c3%a5%d7%a1%a6%a5%a4%a5%d9%a5%f3%a5%c8%c8%ce%c7%e4%be%f0%ca%f3',
+    'd/%a5%ad%a5%e3%a5%f3%a5%da%a1%bc%a5%f3%a1%a6%b4%fc%b4%d6%b8%c2%c4%ea%a5%b7%a5%e7%a5%c3%a5%d7%a1%a6%a5%a4%a5%d9'
+    '%a5%f3%a5%c8%c8%ce%c7%e4%be%f0%ca%f3',
     # 同・終了分
-    'd/%a5%ad%a5%e3%a5%f3%a5%da%a1%bc%a5%f3%a1%a6%b4%fc%b4%d6%b8%c2%c4%ea%a5%b7%a5%e7%a5%c3%a5%d7%a1%a6%a5%a4%a5%d9%a5%f3%a5%c8%c8%ce%c7%e4%be%f0%ca%f3%a1%ca%bd%aa%ce%bb%ca%ac%a1%cb',
+    'd/%a5%ad%a5%e3%a5%f3%a5%da%a1%bc%a5%f3%a1%a6%b4%fc%b4%d6%b8%c2%c4%ea%a5%b7%a5%e7%a5%c3%a5%d7%a1%a6%a5%a4%a5%d9'
+    '%a5%f3%a5%c8%c8%ce%c7%e4%be%f0%ca%f3%a1%ca%bd%aa%ce%bb%ca%ac%a1%cb',
 ]
 # prepend a base url
 IGNORE_URLS = list(map(lambda ignore: BASE_URL + ignore, IGNORE_URLS))
+
 
 def main():
     # get links from menubar
@@ -49,17 +51,20 @@ def main():
         print_item(item, i=i)
         save_item(item)
 
+
 def get_urls_all():
     url = BASE_URL + 'd/MenuBar1'
     soup = get_soup(url)
     urls = [a['href'] for a in soup.select('#content_block_2 li a')[2:]]
     return urls
 
+
 def get_urls_new():
     url = BASE_URL + 'l/'
     soup = get_soup(url)
     urls = [a['href'] for a in soup.select('ul.page-list > li > a')]
     return urls
+
 
 def print_item(item, i=None):
     """
@@ -69,13 +74,15 @@ def print_item(item, i=None):
     if i is not None:
         print('#', i)
     for k, v in item.items():
-        print('- {}: {}'.format(k,repr(v)))
+        print('- {}: {}'.format(k, repr(v)))
+
 
 def save_item(item):
     # use page url as id
-    id = '/'.join(item['url'].split('/')[4:])
-    db.update({'_id': id}, {'$set': item}, upsert=True)
-        
+    item_id = '/'.join(item['url'].split('/')[4:])
+    db.update({'_id': item_id}, {'$set': item}, upsert=True)
+
+
 def get_soup(url):
     """
     get BeautifulSoup object of the url page
@@ -85,6 +92,7 @@ def get_soup(url):
         return None
     return BeautifulSoup(r.text, 'lxml')
 
+
 def parse_page(url):
     """
     analyze the url page and return informations
@@ -92,7 +100,7 @@ def parse_page(url):
     item = {}
     try:
         soup = get_soup(url)
-    except:
+    except Exception as e:
         return None
     if not soup:
         return None
@@ -100,7 +108,7 @@ def parse_page(url):
     # get page meta data
     item['url'] = url
     item['updated_time'] = parse(soup.select('meta[name=generated]')[0]['content'])
-    
+
     soup = soup.select('#page-body')[0]
 
     # get name
@@ -111,7 +119,7 @@ def parse_page(url):
                 break
 
     # get maker name
-    m = re.search(r'(?:発売元|出版社)(?::|：)(\S+)\s*?(?:販売元)?', soup.text)
+    m = re.search(r'(?:発売元|出版社)[:：](\S+)\s*?(?:販売元)?', soup.text)
     if m:
         item['maker'] = m.group(1).replace('株式会社', '').strip()
     else:
@@ -119,19 +127,19 @@ def parse_page(url):
 
     # get tweet ids
     item['tweet_urls'] = [t.select('a')[-1]['href'] for t in soup.select('.embedded-tweet') if t.select('a')]
-        
+
     # remove embeded tweets
     for div in soup.select('.embedded-tweet'):
-        div.extract() 
+        div.extract()
 
-    # remove headers
+        # remove headers
     titles = [div for div in soup.select('div')
               if div.has_attr('class') and re.search(r'title-', ' '.join(div['class']))]
     for title in titles:
         title.extract()
-    
+
     # get release_date
-    m = re.search(r'((?:(?:(\d{4})年)?(\d{,2})月(?:(\d{,2})日)?.*?(?:(上|中|下)旬)?.*?)|発売.+?不明)', soup.text)
+    m = re.search(r'((?:(?:(\d{4})年)?(\d{,2})月(?:(\d{,2})日)?.*?(?:([上中下])旬)?.*?)|発売.+?不明)', soup.text)
     if m:
         # if release date id '不明'
         if '不明' in m.group(1):
@@ -156,15 +164,16 @@ def parse_page(url):
 
     # get tags
     item['tags'] = [tag.text.strip() for tag in soup.select('#page-tags .tags')]
-        
+
     return item
+
 
 if __name__ == '__main__':
     # parse args
     parser = argparse.ArgumentParser()
     parser.add_argument('page', choices=['all', 'new'])
     args = parser.parse_args()
-     
+
     # prepare database
     c = get_mongo_client()
     db = c.kinpri_goods_wiki.items
